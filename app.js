cf9503651c889c1c124dedc70593e432be04a877
Angel Alegre <aalegre.qa@gmail.com>
2023-06-21 12:02:14 -0500

    add midleware

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("Entro en el midleware");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("Antoher midleaware");
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
