62b9b2098d7d9f2803b765e71c9c211a41ae2299
Angel Alegre <aalegre.qa@gmail.com>
2023-06-23 00:14:00 -0500

    manage routes

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const http = require("http");
 const express = require("express");
 
 const app = express();
+//midleware
+app.get("/favicon.ico", (req, res) => res.status(204));
 
-app.use((req, res, next) => {
-  console.log("Entro en el midleware");
-  next();
+app.use("/add-product", (req, res, next) => {
+  console.log("Antoher midleaware 2");
+  res.send("<h1>Add product</h1>");
 });
 
-app.use((req, res, next) => {
+app.use("/", (req, res, next) => {
   console.log("Antoher midleaware");
   res.send("<h1>Hello tiranitar</h1>");
 });
