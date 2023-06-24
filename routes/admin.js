62487618cdec4ae274a29e25e308ad18432c6456
Angel Alegre <aalegre.qa@gmail.com>
2023-06-24 09:18:07 -0500

    add ruta general

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,12 @@
 const path = require("path");
 const express = require("express");
 const router = express.Router();
+const rootDir = require("../util/path");
 
 //  /admin/product => GET
 router.get("/add-product", (req, res, next) => {
   //console.log("Antoher midleaware 2");
-  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 //  /admin/add-product => POST
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,10 @@
 const path = require("path"); // para manejar rutas del fs mas facil
 const express = require("express");
 const router = express.Router();
+const rootDir = require("../util/path");
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
   //__dirname es para el camino desde la raiz, ../ es para sali y lo demas es la ubicacion
 });
 
