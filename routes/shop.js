da0b19a3f39c33e4750d11269aa15dffc28d8a7d
Angel Alegre <aalegre.qa@gmail.com>
2023-06-24 08:01:29 -0500

    add html en midlewares q manejan las rutas

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,10 @@
+const path = require("path"); // para manejar rutas del fs mas facil
 const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  console.log("Antoher midleaware");
-  res.send("<h1>Hello tiranitar</h1>");
+  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+  //__dirname es para el camino desde la raiz, ../ es para sali y lo demas es la ubicacion
 });
 
 module.exports = router;
